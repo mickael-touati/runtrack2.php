1c1d6382edd461fe499b1452fcb896f3b98a126e
Mickael Touati <mickael.touati@laplateforme.io>
2026-01-05 16:30:29 +0100

    jour01 job01: premier variable

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+$str = "La plateforme";
+echo $str;
+
+$str2 = "Vive";
+$str3 = "!";
+echo $str ." ". $str2 ." ". $str3;
+
+$val = "6":
+echo $val ."\n";
+
+$val = $val + 4;
+
+echo $val;
+
+$myBool = true;
+echo $myBool ? 'true' : 'false';
+
+$myBool = false;
+echo $myBool ? 'true' : 'false';
+?>
+
