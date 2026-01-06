fa4152492e9c8bef3f5292f961358eae207bce3a
Mickael Touati <mickael.touati@laplateforme.io>
2026-01-06 10:43:08 +0100

    tableau html en php

---
+++ jour01/job03/index.php
@@ -0,0 +1,40 @@
+<?php
+$int = "2";
+$string = "bonjour";
+$float = 3.14;
+$bool = true;
+
+echo "<table border='1'>";
+
+echo "<tr>
+        <th>Type</th>
+        <th>Nom</th>
+        <th>Valeur</th>
+    </tr>";
+
+echo "<tr>
+        <td>boolean</td>
+        <td>bool</td>
+        <td>$bool</td>
+    </tr>";
+
+echo "<tr>
+        <td>float</td>
+        <td>float</td>
+        <td>$float</td>
+    </tr>";
+ 
+echo "<tr>
+        <td>string</td>
+        <td>string</td>
+        <td>$string</td>
+    </tr>";
+
+echo "<tr>
+        <td>int</td>
+        <td>int</td>
+        <td>$int</td>
+    </tr>";
+
+echo "</table>"
+?>
