d55f19980fef171429ffcc3981c29701e25217a9
Mickael Touati <mickael.touati@laplateforme.io>
2026-01-12 10:02:02 +0100

    fonction avec 3 parametre qui retourne le resultat

---
+++ jour05/job04/index.php
@@ -0,0 +1,17 @@
+<?php
+// Créez une fonction nommée “calcule()” qui prend 3 paramètres :
+// ➔ le premier est un nombre,
+// ➔ le deuxième est un caractère contenant le type d’opération (+ ,-,*, /,%)
+// ➔ le troisième est un nombre
+// La fonction doit retourner le résultat de l’opération.
+
+$a = 18;
+$b = 12;
+
+function calcul($a, $b) {
+
+    return $a + $b;
+}
+
+$somme = calcul($a, $b)
+?>
